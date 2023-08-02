2cb863d813c7eaf3fa9e98df2ee41a0a1b79d7fc
jinseong.cho <jinseong.cho@linecorp.com>
2023-08-02 13:25:06 +0900

    add amino and progo signing example

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { SignClient } from '@walletconnect/sign-client';
 import { Web3Modal } from '@web3modal/standalone';
 import './App.css'
 
+import { Registry, encodePubkey, makeAuthInfoBytes, makeSignDoc } from "@cosmjs/proto-signing";
+import { defaultRegistryTypes } from '@cosmjs/stargate';
+import { makeSignDoc as makeAminoSignDoc} from "@cosmjs/amino"
 
 // Your dapp's Project ID from https://cloud.walletconnect.com/
 const WC_PROJECT_ID = '9e1152b9dc0318eea105dc31238fbc00';
@@ -44,6 +47,7 @@ function initSignClient() {
             web3modal.closeModal();
             setSession(wcSession);
             const bech32 = parseAccount(wcSession.namespaces.cosmos.accounts[0]).bech32;
+            console.log(wcSession.namespaces.cosmos.accounts[0]);
             setAddress(bech32);
         })();
     }, [])
@@ -68,7 +72,7 @@ function parseAccount(account) {
 }
 
 function App() {
-    const [msgToSign, setMsgToSign] = useState('Any text');
+    const [msgToSign, setMsgToSign] = useState('0');
     const [signature, setSignature] = useState(null);
     const [dynamicLinkBase, setDynamicLinkBase] = useState("https://dosivault.page.link/qL6j");
 
@@ -84,18 +88,118 @@ function App() {
     }
 
     async function handleSignArbitraryMsg() {
-        const resp = await signClient.request({
-            chainId: CAIP_BLOCKCHAIN_ID,
-            request: {
-                method: "cosmos_sign_free_message",
-                params: {
-                    msg: msgToSign,
-                    signerAddress: address
-                }    
-            },
-            topic: session.topic
-        });
-        setSignature(resp.signature.signature);
+        const LCD_ENDPOINT= 'https://lbs-finschianw.line-apps.com'
+        const localhost = 'http://localhost:1317'
+        const url = `${localhost}/cosmos/auth/v1beta1/accounts/${address}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        const sequence = data.account.sequence;
+        const accountNumber = data.account.account_number;
+
+        // with amino signing
+        {
+            const sendMsg = {
+                type: "cosmos-sdk/MsgSend",
+                value: {
+                  amount: [
+                    {
+                      amount: msgToSign,
+                      denom: 'cony',
+                    },
+                  ],
+                  from_address: address,
+                  to_address: address,
+                },
+              };
+
+            const gasPrice = {amount: "0.025", denom: 'cony'}
+            const gasLimit = '100000';
+            const fee = {
+                amount: [gasPrice],
+                gas: gasLimit,
+              };
+            
+            const signDoc = makeAminoSignDoc([sendMsg], fee, CHAIN_ID, "test", accountNumber, sequence);
+
+            const params = {
+                signerAddress: address,
+                signDoc: signDoc
+            }
+            console.log(session)
+            console.log(params)
+            const resp = await signClient.request({
+                topic: session.topic,
+                chainId: CAIP_BLOCKCHAIN_ID,
+                request: {
+                    method: "cosmos_signAmino",
+                    params: params
+                },
+            });
+            setSignature(resp.signature.signature);
+        }
+                
+        // with proto signing
+        {
+            const accountResponse = await signClient.request({
+                chainId: CAIP_BLOCKCHAIN_ID,
+                request: {
+                    "method": "cosmos_getAccounts",
+                    "params": {}
+                },
+                topic: session.topic
+            })
+
+            const pubkey = encodePubkey({
+                type: "tendermint/PubKeySecp256k1",
+                value: accountResponse[0].pubkey
+            })
+
+            const registry = new Registry(defaultRegistryTypes);
+
+            const sendMsg = {
+                typeUrl: "/cosmos.bank.v1beta1.MsgSend",
+                value: {
+                fromAddress: address,
+                toAddress: address,
+                amount: [{
+                    amount: msgToSign,
+                    denom: 'cony'}],
+                },
+            };
+
+            const txBodyEncodeObject = {
+                typeUrl: "/cosmos.tx.v1beta1.TxBody",
+                value: {
+                    messages: [sendMsg],
+                    memo: 'test',
+                },
+            };
+
+            const txBodyBytes = registry.encode(txBodyEncodeObject);
+
+            const gasPrice = {amount: "0.025", denom: 'cony'}
+            const gasLimit = 100_000;
+            
+            const authInfoBytes = makeAuthInfoBytes([{ pubkey, sequence }], [gasPrice], gasLimit)
+            const signDoc = makeSignDoc(txBodyBytes, authInfoBytes, CHAIN_ID, Number(accountNumber));
+
+            const params = {
+                signerAddress: address,
+                signDoc: signDoc
+            }
+
+            console.log(session)
+            console.log(params)
+            const resp = await signClient.request({
+                topic: session.topic,
+                chainId: CAIP_BLOCKCHAIN_ID,
+                request: {
+                    method: "cosmos_signDirect",
+                    params: params
+                },
+            });
+            setSignature(resp.signature.signature);
+        }
     }
 
     return (
